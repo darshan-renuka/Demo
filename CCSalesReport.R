74f836ebe71b2f7bdae35ee4d04b9d98a8a72317
Mruga Parikh <37287541+mdp1990@users.noreply.github.com>
2018-04-11 11:45:51 +0530

    Update CCSalesReport.R

--- CCSalesReport.R
+++ CCSalesReport.R
@@ -1,8 +1,6 @@
 library(ggplot2)
 CCSalesDF = read.csv("/Users/darshanairfi/Documents/GitHub/Data-analytics/CCSalesPerformance/output/Temp.csv")
 str(CCSalesDF)
-
-
 ggplot(CCSalesDF, aes(x=ID)) + geom_histogram(aes(alpha=0.5, jitter=0.1))
 
 
