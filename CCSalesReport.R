009db75d3bcd071c3d76ed0c77cd995b18a4e12d
Darshan Gangadhara Renuka <darshan-renuka@users.noreply.github.com>
2018-04-11 11:38:18 +0530

    Demo
    Testing repo

---
+++ CCSalesReport.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+CCSalesDF = read.csv("/Users/darshanairfi/Documents/GitHub/Data-analytics/CCSalesPerformance/output/Temp.csv")
+str(CCSalesDF)
+
+
+ggplot(CCSalesDF, aes(x=ID)) + geom_histogram(aes(alpha=0.5, jitter=0.1))
+
+
